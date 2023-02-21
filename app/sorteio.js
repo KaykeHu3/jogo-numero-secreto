e9af06241669e3c18db25f5def60aee267b63687
Kayke de Paula Menezes <110184998+KaykeHu3@users.noreply.github.com>
2023-02-21 10:40:20 -0300

    publicando projeto

---
+++ app/sorteio.js
@@ -0,0 +1,12 @@
+const menorValor = 1;
+const maiorValor = 100;
+const numeroSecreto = gerarNumeroAleatorio ();
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * maiorValor + 1);   
+}
+
+const elementoMenorValor = document.querySelector("#menor-valor");
+const elementoMaiorValor = document.querySelector("#maior-valor");
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
